ef473f89cfd496bc5097a33ea65c55053a29a64b
alumnoscvma <118540321+alumnoscvma@users.noreply.github.com>
2022-11-18 07:53:26 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+    apiKey: "AIzaSyB2SZm7sgLekjEDdfpujYNJtdMUbSIdmTU",
+  authDomain: "alumnoscvma0.firebaseapp.com",
+  projectId: "alumnoscvma0",
+  storageBucket: "alumnoscvma0.appspot.com",
+  messagingSenderId: "793334395330",
+  appId: "1:793334395330:web:5c908190800b502104265b",
+  measurementId: "G-CCGNVY784J"
 });
